a85da53295b80ff24b1345564013c96809a2493d
Saksham Gupta <90046016+0xSaksham@users.noreply.github.com>
2024-03-26 10:32:44 +0000

    Tree Data Structure Ready!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
 fn main() {
-    println!("Jai Shree Ram!");
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+    // println!("\nLeaf Parent: {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "Strong Leaf: {}, Weak Leaf: {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!(
+            "Strong Branch: {}, Weak Branch: {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+
+        println!(
+            "Strong Leaf: {}, Weak Leaf: {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+    }
+    println!("\nLeaf Parent: {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "Strong Leaf: {}, Weak Leaf: {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 }
